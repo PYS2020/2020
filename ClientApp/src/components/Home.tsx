65996e3980951b93a3a0b22a6a9b2454cc599d33
yongshan <pangyongshan@hotmail.com>
2020-05-31 12:14:54 +0800

    Test VS code

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => (
     <h1>Hello, world!</h1>
     <p>Welcome to your new single-page application, built with:</p>
     <ul>
-      <li><a href='https://get.asp.net/'>ASP.NET Core 5.0</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
+      <li><a href='https://get.asp.net/'>ASP.NET Core test 5.0</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
       <li><a href='https://facebook.github.io/react/'>React</a> and <a href='https://redux.js.org/'>Redux</a> for client-side code</li>
       <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
     </ul>
